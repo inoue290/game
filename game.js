31312a7f5fb6bb7673271069ae10f56c7fb6e49a
inoue290 <36177217+inoue290@users.noreply.github.com>
2025-03-28 10:13:36 +0900

    Update game.js

--- game.js
+++ game.js
@@ -19,14 +19,6 @@ let players = {};  // 他のプレイヤーを管理するオブジェクト
 let cursors, socket, playerId;
 let attackEffectDuration = 500;  // 攻撃エフェクトの表示時間（ミリ秒）
 
-function setUserInUrl(playerId) {
-    const currentUrl = window.location.href.split('?')[0];  // ?以降を削除してベースURLを取得
-    const newUrl = currentUrl + '?user=' + playerId;  // ?user=プレイヤーIDを追加
-    // URLを更新してページをリロード
-    window.history.pushState({}, '', newUrl);  // 新しいURLを履歴に追加
-    window.location.reload();  // ページをリロード
-}
-
 function preload() {
     this.load.image('background', 'assets/background.png');  // 背景画像の読み込み
     this.load.image('player', 'assets/player.png');  // プレイヤー画像の読み込み
@@ -46,8 +38,7 @@ function create() {
         if (data.type === 'welcome') {
             // サーバーからプレイヤーIDが送られてきた場合
             playerId = data.id;
-            // playerIdを使ってURLに追加し、ページをリロード
-            setUserInUrl(playerId);
+
             player = this.physics.add.sprite(Phaser.Math.Between(100, 700), Phaser.Math.Between(100, 500), 'player');
             player.setCollideWorldBounds(true);  // 画面外に出ないように設定
             player.setBounce(1);  // 画面の端に当たったときの反発を有効にする
