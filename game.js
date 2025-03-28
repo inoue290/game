47e61d06b97ba734dcf865f938fb9ba75512e238
inoue290 <36177217+inoue290@users.noreply.github.com>
2025-03-28 16:29:40 +0900

    Update game.js

--- game.js
+++ game.js
@@ -23,6 +23,10 @@ window.onload = function () {
     }
 };
 
+window.addEventListener('resize', function() {
+    game.scale.resize(window.innerWidth, window.innerHeight);
+});
+
 // 20桁のランダムな英数字を生成する関数
 function generateRandomString(length) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
